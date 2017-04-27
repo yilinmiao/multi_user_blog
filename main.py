5c4f77513dd564eb095d54f454bf88dbac4c7035
yilinmiao <yi@Yis-MacBook-Air.local>
2017-04-26 23:24:23 -0700

    Refactored models and handlers, modified html

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ import hashlib
 import hmac
 from string import letters
 
+from models.user import User
+from models.post import Post
+
+#from handlers.signup import Signup
+
 import webapp2
 import jinja2
 
@@ -83,69 +88,6 @@ def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
     response.out.write(post.content)
 
-##### user stuff
-def make_salt(length = 5):
-    return ''.join(random.choice(letters) for x in xrange(length))
-
-def make_pw_hash(name, pw, salt = None):
-    if not salt:
-        salt = make_salt()
-    h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s,%s' % (salt, h)
-
-def valid_pw(name, password, h):
-    salt = h.split(',')[0]
-    return h == make_pw_hash(name, password, salt)
-
-def users_key(group = 'default'):
-    return db.Key.from_path('users', group)
-
-class User(db.Model):
-    name = db.StringProperty(required = True)
-    pw_hash = db.StringProperty(required = True)
-    email = db.StringProperty()
-
-    @classmethod
-    def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
-
-    @classmethod
-    def by_name(cls, name):
-        u = User.all().filter('name =', name).get()
-        return u
-
-    @classmethod
-    def register(cls, name, pw, email = None):
-        pw_hash = make_pw_hash(name, pw)
-        return User(parent = users_key(),
-                    name = name,
-                    pw_hash = pw_hash,
-                    email = email)
-
-    @classmethod
-    def login(cls, name, pw):
-        u = cls.by_name(name)
-        if u and valid_pw(name, pw, u.pw_hash):
-            return u
-
-
-##### blog stuff
-#data store
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
-
-class Post(db.Model):
-    user_id = db.IntegerProperty(required=True)
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-    
-    def render(self):
-        #everytime there is a new line, the new line needs a linebreak
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p = self)
-
 class BlogFront(BlogHandler):
     def get(self):
         #look up all the posts by creation time
@@ -154,41 +96,6 @@ class BlogFront(BlogHandler):
         #GQL
         #posts = db.GqlQuery("select * from Post order by created desc limit 10")
         self.render('front.html', posts = posts)
-
-class PostPage(BlogHandler):
-    def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = db.get(key)
-
-        if not post:
-            self.error(404)
-            return
-
-        self.render("permalink.html", post = post)
-
-class NewPost(BlogHandler):
-    def get(self):
-        if self.user:
-            self.render("newpost.html")
-        else:
-            self.redirect("/login")
-
-    def post(self):
-        if not self.user:
-            self.redirect('/')
-
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(parent=blog_key(), user_id=self.user.key().id(),
-                     subject=subject, content=content)
-            p.put()
-            self.redirect('/blog/%s' % str(p.key().id()))
-        else:
-            error = "subject and content, please!"
-            self.render("newpost.html", subject=subject, content=content, error=error)
-
 
 #sign up
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
@@ -239,6 +146,40 @@ class Signup(BlogHandler):
 
     def done(self, *a, **kw):
         raise NotImplementedError
+
+class PostPage(BlogHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class NewPost(BlogHandler):
+    def get(self):
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect("/login")
+
+    def post(self):
+        if not self.user:
+            self.redirect('/')
+
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent=blog_key(), user_id=self.user.key().id(),
+                     subject=subject, content=content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
 
 class Register(Signup):
     def done(self):
---
+++ models/post.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import db
+
+##### blog stuff
+#data store
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    user_id = db.IntegerProperty(required=True)
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+    
+    def render(self):
+        #everytime there is a new line, the new line needs a linebreak
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
